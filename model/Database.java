3824684484802b8ec9bc0951c22ecde049824b0f
Laurens Wissels <laurenswissels@iPhonevanGamme.localdomain>
2020-11-19 14:37:50 +0100

    commiting databaseController and fileReader

---
+++ model/Database.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class Database {
+
+    private HashMap<String, String> database;
+
+    public Database() {
+        this.database = new HashMap<>();
+    }
+
+    public HashMap<String, String> getDatabase() {
+        return database;
+    }
+
+    public void printDatabase(){
+        for(String user: this.database.keySet()){
+            System.out.println(this.database.get(user));
+        }
+    }
+}
